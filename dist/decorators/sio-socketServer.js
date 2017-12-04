0236e163b3261a73062ab61c3cf2b8db28db66bd
Matthew Willard <mattw@detroitsoftware.com>
2017-12-04 15:33:00 -0500

    More tests

--- dist/decorators/sio-socketServer.js
+++ dist/decorators/sio-socketServer.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sio_controller_1 = require("./../core/sio-controller");
 function SioSocketServer(target, key) {
-    let _val = this[key];
     let getter = function () {
         return sio_controller_1.SioController.getInstance().rootSocketServer();
     };
--- src/lib/decorators/sio-socketServer.ts
+++ src/lib/decorators/sio-socketServer.ts
@@ -1,7 +1,6 @@
 import {SioController} from './../core/sio-controller'
 export function SioSocketServer(target, key:string) : any
 {
-    let _val = this[key];
     let getter = function(){
         return SioController.getInstance().rootSocketServer();
     }
